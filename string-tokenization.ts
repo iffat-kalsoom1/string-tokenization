9ec1916521d94d1c7066daded985f616284c1d17
Muhammadpen <splasheoo@gmail.com>
2023-04-12 13:08:51 +0500

    tokenization funtion. added docs and updated readme

--- string-tokenization.ts
+++ string-tokenization.ts
@@ -1,30 +1,57 @@
-const wordTokenization = (str: string) => {
-  let word = '';
-  let wordTokenizationResults = '';
-  let i : number
-  for (i = 0; i < str.length; i++) {
-    word = word + str[i];
-    wordTokenizationResults = wordTokenizationResults + ' ' + word;
+/**
+Tokenize a given string.
+@param {string} string - The string to be tokenized.
+@param {boolean} shouldTokenizeWords - Indicates whether to tokenize individual words or not.
+@returns {string[]} - An array of tokens.
+@example
+// Tokenize a string
+tokenize('Hello world! This is an example.', false);
+// Output: ["hello", "world!", "this", "is", "an", "example."]
+
+// Also tokenize individual words in a string
+tokenize('Hello world! This is an example.', true);
+/* Output: [
+    "h", "he", "o", "hel", "lo", "hell", "llo", "hello",
+    "ello", "", "w", "wo","!", "wor", "d!", "worl",
+    "ld!", "world", "rld!", "world!","orld!", "t", "th",
+    "s","thi", "is", "this", "his","i", "a", "an", "n",
+    "e", "ex", ".", "exa","e.",  "exam", "le.", "examp",
+    "ple.", "exampl", "mple.", "example","ample.", "example.", "xample."
+    ]
+*/
+export function tokenize(
+  string: string,
+  shouldTokenizeWords: boolean,
+): string[] {
+  const tokensArray = Array.from(new Set(string.toLowerCase().split(' ')))
+
+  if (!shouldTokenizeWords) return tokensArray
+
+  if (shouldTokenizeWords) {
+    let resultTokens = []
+    tokensArray.forEach(token => {
+      let wordTokens = ''
+      let reverseWordToken = ''
+
+      const tokenLength = token.length
+
+      for (let i = 0; i < tokenLength; i++) {
+        wordTokens = wordTokens + token[i]
+        resultTokens.push(wordTokens)
+
+        if (token[tokenLength - i]) {
+          reverseWordToken = reverseWordToken + token[tokenLength - i]
+          let revToken = ''
+          for (let i = reverseWordToken.length - 1; i >= 0; i--) {
+            revToken += reverseWordToken[i]
+          }
+          resultTokens.push(revToken)
+        }
+
+        if (i + 1 === tokenLength) wordTokens = ''
+      }
+      resultTokens.push(wordTokens)
+    })
+    return Array.from(new Set(resultTokens))
   }
-  return wordTokenizationResults;
-};
-const removeRepetitions = (str: string) => {
-  const spaceSeparatedWords = str.split(' ');
-  spaceSeparatedWords.forEach((word, index) => {
-    let i : number;
-    for (i = index + 1; i < spaceSeparatedWords.length; i++) {
-      if (spaceSeparatedWords[i] == word) spaceSeparatedWords[i] = '';
-    }
-  });
-  const noEmptyStrings = spaceSeparatedWords.filter((str) => str !== '');
-  return noEmptyStrings.join(' ');
-};
-export function stringTokenization(str: string) : string {
-  const spaceSeparatedWords = str.split(' ');
-  let stringTokenizationResults = '';
-  spaceSeparatedWords.forEach((word) => {
-    const tokenizedWordString = wordTokenization(word);
-    stringTokenizationResults = stringTokenizationResults + '' + tokenizedWordString;
-  });
-  return removeRepetitions(stringTokenizationResults.toLowerCase());
-};
+}
